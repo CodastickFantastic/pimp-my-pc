9aeab56ccb7eb02bcc4c0a176b1e31f1a2eb86d2
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2022-11-25 11:10:48 +0100

    Added rotate GPU posibility

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let isVertical = true //Used by rotateGpu() function
+let displayComponents = document.getElementById("pc-components")
+let displayComponentButton = document.querySelectorAll("button")
+
 const componentsList = {
     motherboards : [
         {
@@ -25,6 +29,7 @@ const componentsList = {
         {
             name: "GPU1",
             img: "img/PC_Components/GPUs/GPU-Aorus.png",
+            img2: "img/PC_Components/GPUs/GPU-Aorus-Normal.png",
             price: "4000.00 zł"
         },
         {
@@ -115,9 +120,6 @@ let myBuild = [
     {myMotherBoard:""},
 ]
 
-let displayComponents = document.getElementById("pc-components")
-let displayComponentButton = document.querySelectorAll("button")
-
 function displaySpecificComponentsList(returnFromAddComponent) {
     // Get component name to display from HTML sheet
     let componentType = (typeof returnFromAddComponent === "string") ? returnFromAddComponent : this.getAttribute("componentType")
@@ -150,12 +152,54 @@ function addComponent(i, componentType){
     // Add specific component to PC
     let componentPicture = componentsList[componentType][i].img
     let componentImg = document.createElement("img")
+    let componentDiv = document.getElementById(componentType)
+    let pcWorkbench = document.getElementById("pc-img")
+    
     componentImg.setAttribute("src", componentPicture)
     componentImg.addEventListener("click", () => displaySpecificComponentsList(componentType))
     
-    let componentDiv = document.getElementById(componentType)
-    componentDiv.append(componentImg)    
+    //Add posobility to rotate GPU
+    if(componentType === "gpus"){
+        componentImg.setAttribute("id", i+componentType)
+        let rotateButton = document.createElement("button")
+        let rotatePicture = document.createElement("img")
+        let rotateButtonDescription = document.createElement("p")
+        let checkButtonExistance = document.getElementById("rotate-gpu");
+        rotateButtonDescription.innerHTML = "Change GPU Position"
+        
+        
+        if( checkButtonExistance == undefined){ 
+            console.log("nie widać ") 
+            rotateButton.setAttribute("id", "rotate-gpu")
+            rotateButton.addEventListener("click", () => rotateGPU(i, componentType))
+
+            rotatePicture.setAttribute("src", "img/Icons/rotate.png" )
+            
+            rotateButton.append(rotatePicture)
+            rotateButton.append(rotateButtonDescription)
+            pcWorkbench.append(rotateButton)
+        }
+    }
+    componentDiv.append(componentImg)
+}
+
+
+function rotateGPU(i, componentType){
+    let gpuImgDestroy = document.getElementById(i+componentType)
+    let normalGpuDiv = document.getElementById("normal-gpu-standing")
+    let normalGpuPositionImg = document.createElement("img")
     
+    if (isVertical == true){
+        gpuImgDestroy.remove()
+        normalGpuPositionImg.setAttribute("src", componentsList[componentType][i].img2)
+        normalGpuPositionImg.addEventListener("click", () => displaySpecificComponentsList(componentType))
+        normalGpuDiv.append(normalGpuPositionImg)
+        isVertical = false
+    } else {
+        normalGpuDiv.removeChild(normalGpuDiv.lastChild);
+        addComponent(i, componentType)
+        isVertical = true
+    }
 }
 
 
